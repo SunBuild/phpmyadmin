a67cee20604a63d6f602cc36087982641485f22a
Sunitha Muthukrishna <mksunitha@gmail.com>
2016-09-19 15:21:29 -0700

    Update ping-azure-webapp.php

--- ping-azure-webapp.php
+++ ping-azure-webapp.php
@@ -1,24 +1,13 @@
 <?php
 
-$current_webapp = 'http://'.$_SERVER['HTTP_HOST'];
-$response = get_web_page($current_webapp);
+$url = 'http://'.$_SERVER['HTTP_HOST'];
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
 
-function get_web_page($url) {
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,   // return web page
-        CURLOPT_HEADER         => false,  // don't return headers
-        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-        CURLOPT_ENCODING       => "",     // handle compressed
-        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-        CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-        CURLOPT_TIMEOUT        => 120,    // time-out on response
-    ); 
-
-    $ch = curl_init($url);
-    curl_setopt_array($ch, $options);
-    $content  = curl_exec($ch);
-    curl_close($ch);
-}
 
 ?>
